e0674952a2c7dba117d3cdc196f21e25d9c88d50
J-B-21 <evinindiomo@gmail.com>
2026-05-04 23:37:50 +0100

    Feat: Connect to postgres

---
+++ backend/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './src/config/migrations'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './src/config/migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: './src/config/migrations'
+    }
+  }
+
+};
